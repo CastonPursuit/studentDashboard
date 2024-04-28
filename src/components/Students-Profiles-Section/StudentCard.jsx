828f5ff996dcc23ae1f4b6f86ec91f6d86f31689
CLeeBenjamin <castonboyd@gmail.com>
2024-04-28 08:52:14 -0400

    refactor: StudentCard component, implement cohort expansion feature

--- src/components/Students-Profiles-Section/StudentCard.jsx
+++ src/components/Students-Profiles-Section/StudentCard.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './student_card.scss';
-import studentData from '../../data/data.json'
+import ExpandedCard from './ExpandedCard';
 
-const StudentCard = () => {
-  const {username, dob, profilePhoto } = studentData[0];
-  const  {preferredName, surname, middleName} = studentData[0].names
+const StudentCard = ({student}) => {
+  const {username, dob, profilePhoto, cohort, certifications, codewars } = student;
+  const {preferredName, surname, middleName} = student.names
+  const [isExpanded, setExpanded] = useState(false); 
+
+  const expansionToggle = () => {
+    setExpanded(value => !value);
+  }
   return (
   <div className='student-card'>
     <img className='student-card__image' src={profilePhoto} alt='Student Profile' />
@@ -14,10 +19,28 @@ const StudentCard = () => {
         <p className='student-card__username'>{username}</p>
         <p className='student-card__dob'>{`Birthday: ${dob}`}</p>
       </div>
-      <button className='student-card__button'>Show More...</button>
+
+      <button onClick={expansionToggle} className='student-card__button'>{isExpanded ? 'Show less...': 'Show more...'}</button>
+      {isExpanded &&
+       ( <ExpandedCard
+
+        assignments={cohort.scores.assignments}
+        assessments={cohort.scores.assessments}
+        projects={cohort.scores.projects}
+        currentTotal={codewars.current.total}
+        currentLastWeek={codewars.current.lastWeek}
+        goalTotal={codewars.goal.total}
+        resume={certifications.resume}
+        linkedin={certifications.linkedin}
+        github={certifications.github}
+        mockInterview={certifications.mockInterview}
+       />)
+       
+       }
     </div>
   </div>
   )
 }
+
 
 export default StudentCard
--- src/components/Students-Profiles-Section/StudentProfileContainer.jsx
+++ src/components/Students-Profiles-Section/StudentProfileContainer.jsx
@@ -5,14 +5,15 @@ import studentData from '../../data/data.json'
 
 const StudentProfileContainer = () => {
   const studentCount = studentData.length;
+
   return ( 
-  <section class="container"> 
-    <header class="container__header"> 
-      <h1 class="container__title">All Students</h1>
-      <h1 class="container__count">Total Students: <span className='container__number'> {studentCount} </span> </h1>
+  <section className="container"> 
+    <header className="container__header"> 
+      <h1 className="container__title">All Students</h1>
+      <h1 className="container__count">Total Students: <span className='container__number'> {studentCount} </span> </h1>
     </header>
-    <ul class="container__student-list">
-      <li class="container__student-item"><StudentCard/></li>
+    <ul className="container__student-list">
+      {studentData.map(student => (<li className="container__student-item"><StudentCard student={student}/></li> ))} 
     </ul>
   </section>
   )
